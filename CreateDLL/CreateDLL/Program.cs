d62a1235c82f73defd172bbee181a4f3b22268e0
Alex22891 <103401307+Alex22891@users.noreply.github.com>
2022-11-21 16:53:30 +0900

    Add files via upload

---
+++ CreateDLL/CreateDLL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyLib;
+
+namespace CreateDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Global function = new Global();
+
+            function.SayHello("Александр");
+            function.Question();
+            Console.WriteLine($"Через пять лет вам будет: {function.Age(18)}");
+            Console.WriteLine($"Сумма в результате умножения будет равна: {function.Sum(5,5)}");
+            Console.WriteLine($"Сумма в результате деления будет равна: {function.division(5,5)}");
+        }
+    }
+}
---
+++ CreateDLL/MyLib/Global.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Global
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Здравствуйте, {name}");
+        }
+
+        public void Question()
+        {
+            Console.WriteLine("Как твои дела?");
+        }
+
+        public int Age(int x)
+        {
+            return x + 5;
+        }
+
+        public float Sum(float x, float y)
+        {
+            return x * y;
+        }
+
+        public double division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
