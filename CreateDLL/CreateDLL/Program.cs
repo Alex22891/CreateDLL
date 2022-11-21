7dd4709580fcb329fd3575c83e74d2f820c4cf81
Alex22891 <103401307+Alex22891@users.noreply.github.com>
2022-11-21 15:05:00 +0900

    Add files via upload

---
+++ CreateDLL/CreateDLL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyLib;
+
+namespace CreateDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Global function = new Global();
+
+            function.SayHello("Александр");
+            Console.WriteLine($"Через пять лет вам будет: {function.Age(18)}");
+        }
+    }
+}
---
+++ CreateDLL/MyLib/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Global
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Здравствуйте, {name}");
+        }
+
+        public int Age(int x)
+        {
+            return x + 5;
+        }
+    }
+}
